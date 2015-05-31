d94e0e9cd094f9b1c09e4feb0f462f739cc49a44
mrfawy <mrfawy@yahoo.com>
2015-05-30 21:54:29 -0500

    shadow skelton

--- ShadowEncryptor.py
+++ ShadowEncryptor.py
@@ -1,5 +1,6 @@
 from EnigmaMapProducer import EnigmaMapProducer
 from ModernEnigma import ModernEnigma
+from queues import Queue
 
 """
 Encrypts/decrypts a message
@@ -10,7 +11,42 @@ it creates queues and start machine producers , for each input byte it collect m
 
 """
 class ShadowEncryptor:
-    def __init__(self,):
-        pass
+    def __init__(self,machineShadowList):
+        self.machineShadowList=machineShadowList
+        self.initProducers()
+        self.initReflector()
+
+    def initProducers(self):
+        size=self.machineShadowList.len
+        self.queues=[]
+        self.producers=[]
+        for i in range(size):
+            q=Queue()
+            producer=EnigmaMapProducer(q,self.machineShadowList[i])
+            self.producers.append(producer)
+            self.queues.append(q)
+            producer.start()
+    def initReflector(self):
+        self.reflector=None
+
+
+    def processStream(self,stream):
+        output=[]
+        for b in stream:
+            substituonMapList=[]
+            i=b
+            for q in self.queues:
+                substituonMapList.append(q.get(True))
+            for substituonMap in substituonMapList:
+                y=substutionMap[i]
+            #y=self.reflector.signalIn([y]
+            for substituonMap in reversed(substituonMapList):
+                y=substituonMap[y]
+            output.append[y]
+        for producer in self.producers:
+            producer.stop()
+
+
+
 
 
